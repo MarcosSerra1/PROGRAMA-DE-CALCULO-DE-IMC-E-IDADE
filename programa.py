c5a4db4cf236c3450bd64e3c34d4ff031cefd66c
Marcos Serra <marcosserra133@gmail.com>
2021-11-18 18:08:11 -0300

    Update programa.py

--- programa.py
+++ programa.py
@@ -1,6 +1,7 @@
 """
 Programa para calculo de IMC e idade.
 """
+
 print('=' * 40)
 print()
 print('Caro ussuario colocar "." no lugar ","')
@@ -8,7 +9,7 @@ print('-' * 40)
 nome = str(input('Qual é o seu nome? '))
 ano_nasc = int(input('Em que ano você nasceu? '))
 altura = float(input('Qual é a sua altura? '))
-peso = float(input('Qual é o seu peso?(kg)' ))
+peso = float(input('Qual é o seu peso?(kg)'))
 ano_atu = int(input('Em que ano estamos? '))
 imc = peso / (altura ** 2)
 idade = ano_atu - ano_nasc
